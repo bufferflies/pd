898df1a003d67574e3fe63efef503c722472e5ba
bufferflies <1045931706@qq.com>
2021-05-31 16:24:01 +0800

    move test to cache test
    Signed-off-by: tongjian <1045931706@qq.com>
    Signed-off-by: bufferflies <1045931706@qq.com>

--- pkg/cache/cache_test.go
+++ pkg/cache/cache_test.go
@@ -297,3 +297,38 @@ func (s *testRegionCacheSuite) TestTwoQueueCache(c *C) {
 	c.Assert(ok, IsFalse)
 	c.Assert(val, IsNil)
 }
+
+func (t *testRegionCacheSuite) TestPriorityQueue(c *C) {
+	pq := NewPriorityQueue()
+	pq.Push(1, 1)
+	pq.Push(3, 3)
+	pq.Push(5, 3)
+	pq.Push(2, 2)
+
+	// case1 test getAll ,the highest element should be the first
+	entries := pq.queue.GetAll(-1)
+	c.Assert(len(entries), Equals, 3)
+	c.Assert(entries[0].Priority, Equals, 5)
+	c.Assert(entries[0].Value, Equals, 3)
+
+	// case2 test remove the high element, and the second element should be the first
+	pq.RemoveValues([]interface{}{3})
+	c.Assert(pq.Size(), Equals, 2)
+	entry := pq.Peek()
+	c.Assert(entry.Priority, Equals, 2)
+	c.Assert(entry.Index, Equals, 0)
+	c.Assert(entry.Value, Equals, 2)
+
+	// case3 update entry should be fix
+	pq.Update(entry, 3)
+	entry = pq.Pop()
+	c.Assert(entry.Priority, Equals, 3)
+	c.Assert(entry.Index, Equals, -1)
+	c.Assert(entry.Value, Equals, 2)
+
+	// case4 remove all element
+	pq.RemoveValues([]interface{}{1})
+	c.Assert(pq.Size(), Equals, 0)
+	c.Assert(len(pq.items), Equals, 0)
+	c.Assert(pq.queue.Len(), Equals, 0)
+}
