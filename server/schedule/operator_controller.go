4ff264fc897d38fab6f49e60ca98c3953263777b
bufferflies <1045931706@qq.com>
2021-06-01 16:05:29 +0800

    golint modify
    Signed-off-by: bufferflies <1045931706@qq.com>
    
    Update server/core/store_stats.go
    
    Co-authored-by: ShuNing <nolouch@gmail.com>

--- server/schedule/operator_controller.go
+++ server/schedule/operator_controller.go
@@ -52,7 +52,7 @@ var (
 	PushOperatorTickInterval = 500 * time.Millisecond
 	// StoreBalanceBaseTime represents the base time of balance rate.
 	StoreBalanceBaseTime float64 = 60
-	// operator min finish time,if finish duration less than it,op will be pushed to fast operator queue
+	// FastOperatorFinishTime min finish time,if finish duration less than it,op will be pushed to fast operator queue
 	FastOperatorFinishTime = 10 * time.Second
 )
 
