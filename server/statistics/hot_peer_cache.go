eef1403d66e8c23f56df49039eb73d680b006e57
bufferflies <1045931706@qq.com>
2021-11-10 11:03:26 +0800

    read threshold decrease 1/4
    Signed-off-by: bufferflies <1045931706@qq.com>

--- server/statistics/hot_peer_cache.go
+++ server/statistics/hot_peer_cache.go
@@ -49,9 +49,9 @@ var minHotThresholds = [RegionStatCount]float64{
 	RegionWriteBytes: 1 * 1024,
 	RegionWriteKeys:  32,
 	RegionWriteQuery: 32,
-	RegionReadBytes:  8 * 1024,
-	RegionReadKeys:   128,
-	RegionReadQuery:  128,
+	RegionReadBytes:  8 * 1024 / 4,
+	RegionReadKeys:   128 / 4,
+	RegionReadQuery:  128 / 4,
 }
 
 // hotPeerCache saves the hot peer's statistics.
